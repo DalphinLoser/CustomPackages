b85ff9e079c6fcaaa959cfc5b89e3d88362d7535
DalphinLoser <63314427+DalphinLoser@users.noreply.github.com>
2023-09-21 18:55:33 -0400

    made better
    much better

--- scripts/fetch-latest-release.ps1
+++ scripts/fetch-latest-release.ps1
@@ -1,205 +1,391 @@
 $ErrorActionPreference = 'Stop'
-
-# Function to print a log section header
-Function Write-LogHeader {
+###################################################################################################
+#region Functions
+function Write-LogHeader {
     param (
         [string]$Message
     )
-    Write-Host "`n=== [ $Message ] ===`n"
+    Write-Host "`n=== [ $Message ] ===`n" -ForegroundColor Magenta
 }
-
-Write-LogHeader "Initializing Script"
-
-Function Get-LatestReleaseInfo {
+function Select-Asset {
     param (
-        [string]$repo
+        [array]$p_assets
     )
-    $githubUser = $repo.Split('/')[3]
-    $githubRepo = $repo.Split('/')[4]
-    $latestReleaseUrl = "https://api.github.com/repos/${githubUser}/${githubRepo}/releases/latest"
 
-    # Validate constructed URL
-    if ($latestReleaseUrl -notmatch "https://api.github.com/repos/.*/.*/releases/latest") {
-        Write-Error "Invalid GitHub release URL: $latestReleaseUrl"
-        return $null
+    # Validation check for the assets
+    $f_supportedTypes = @('exe', 'msi', '7z', 'zip', 'msu', 'msp')
+    # Select the first asset that matches a supported type
+    $f_selectedAsset = $p_assets | Where-Object { $_.name -match '\.([^.]+)$' } | Sort-Object { $f_supportedTypes.IndexOf($matches[1]) } | Select-Object -First 1
+    # Validation check for the selected asset
+    if ($null -eq $f_selectedAsset) {
+        Write-Error "No suitable asset (.exe, .msi, .zip, .7z, .msu, .msp) found for the latest release."
+        exit 1
     }
-    
+
+    return $f_selectedAsset
+}
+function Get-RepoDescription {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$p_latestReleaseUrl
+    )
+    # Remove /release/latest from the URL
+    $f_repoInfoUrl = $p_latestReleaseUrl -replace '/releases/latest'
+    Write-Host "Fetching repository description from GitHub... ($f_repoInfoUrl)"
+
     # Fetch and parse latest release data
-    $latestReleaseInfo = (Invoke-WebRequest -Uri $latestReleaseUrl).Content | ConvertFrom-Json
+    $f_repoInfo = (Invoke-WebRequest -Uri $f_repoInfoUrl).Content | ConvertFrom-Json
     
     # Validation check for the API call
-    if ($null -eq $latestReleaseInfo -or $latestReleaseInfo.PSObject.Properties.Name -notcontains 'tag_name') {
+    if ($null -eq $f_repoInfo -or $f_repoInfo.PSObject.Properties.Name -notcontains 'description') {
         Write-Error "Failed to fetch valid release information from GitHub."
-        return $null
+        exit 1
     }
 
-    return $latestReleaseInfo
+    return $f_repoInfo.description
 }
-
-Function Select-Asset {
+function Get-Filetype {
     param (
-        [array]$assets
+        [Parameter(Mandatory=$true)]
+        [string]$p_fileName
     )
-    $supportedTypes = @('exe', 'msi', '7z', 'zip', 'msu', 'msp')
-    $selectedAsset = $assets | Where-Object { $_.name -match '\.([^.]+)$' } | Sort-Object { $supportedTypes.IndexOf($matches[1]) } | Select-Object -First 1
-
-    if ($null -eq $selectedAsset) {
-        Write-Error "No suitable asset (.exe, .msi, .zip, .7z, .msu, .msp) found for the latest release."
-        return $null
+    
+    if ($p_fileName -match '\.(exe|msi|zip|7z|msu|msp)$') {
+        # Return the file type from the file name
+        return $matches[1]
+        } 
+    else { 
+        Write-Error "Unsupported file type: $p_fileName"
+        exit 1
     }
-
-    return $selectedAsset
 }
-
-Function Get-SilentArgs {
+function Get-SilentArgs {
+    # This is admittedly not a great way to handle this.
+    # Maybe run /help or /? on the installer and parse the output?
     [CmdletBinding()]
     param (
         [Parameter(Mandatory=$true)]
         [ValidateSet('exe', 'msi', '7z', 'zip', 'msu', 'msp')]
-        [string]$fileType
+        [string]$p_fileType
     )
 
-    Write-Host "Determining silent installation arguments for $fileType..."
-
-    $silentArgs = ''
+    $f_silentArgs = ''
     
-    switch ($fileType) {
+    switch ($p_fileType) {
         'exe' { 
-            $silentArgs = '/SP- /VERYSILENT /NORESTART'  # Silent installation
+            $f_silentArgs = '/S /s /Q /q /SP- /VERYSILENT /NORESTART /quiet /silent'  # Silent installation
         }
         'msi' { 
-            $silentArgs = '/qn /norestart'  # Quiet mode, no user input, no restart
+            $f_silentArgs = '/quiet /qn /norestart'  # Quiet mode, no user input, no restart
         }
         '7z'  { 
-            $silentArgs = '-y'  # Assume yes on all queries
+            $f_silentArgs = '-y'  # Assume yes on all queries
         }
         'zip' { 
-            $silentArgs = '-y'  # Assume yes on all queries (Note: Not standard for ZIP)
+            $f_silentArgs = '-y'  # Assume yes on all queries (Note: Not standard for ZIP)
         }
         'msu' { 
-            $silentArgs = '/quiet /norestart'  # Quiet mode, no restart
+            $f_silentArgs = '/quiet /norestart'  # Quiet mode, no restart
         }
         'msp' { 
-            $silentArgs = '/qn /norestart'  # Quiet mode, no restart
+            $f_silentArgs = '/qn /norestart'  # Quiet mode, no restart
         }
         default { 
-            Write-Error "Unsupported file type: $fileType"
-            return ''
+            Write-Error "Unsupported file type: $p_fileType"
+            exit 1
         }
     }
 
-    Write-Host "Silent installation arguments for {$fileType}: $silentArgs"
-    return $silentArgs
+    return $f_silentArgs
 }
+function Get-LatestReleaseInfo {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$p_latestReleaseUrl
+    )
+    # Validate constructed URL
+    if ($p_latestReleaseUrl -notmatch "https://api.github.com/repos/.*/.*/releases/latest") {
+        Write-Error "Invalid GitHub release URL: $p_latestReleaseUrl"
+        return $null
+    }
+    
+    # Fetch and parse latest release data
+    $f_latestReleaseInfo = (Invoke-WebRequest -Uri $p_latestReleaseUrl).Content | ConvertFrom-Json
+    
+    # Validation check for the API call
+    if ($null -eq $f_latestReleaseInfo -or $f_latestReleaseInfo.PSObject.Properties.Name -notcontains 'tag_name') {
+        Write-Error "Failed to fetch valid release information from GitHub."
+        exit 1
+    }
 
-
-Write-LogHeader "Fetching Latest Release Info"
-
-# Initialize repository URL
-$repo = "https://github.com/maah/ProtonVPN-win-app"
-
-# Fetch latest release information
-$latestReleaseInfo = Get-LatestReleaseInfo -repo $repo
-if ($null -eq $latestReleaseInfo) {
-    exit 1
+    return $f_latestReleaseInfo
 }
+function New-NuspecFile {
+    param (
+        [Parameter(Mandatory=$true)]
+        [PSCustomObject]$p_Metadata,
 
-write-host "Latest Release URL: $latestReleaseUrl"
+        [Parameter(Mandatory=$true)]
+        [string]$p_packageDir
+    )
 
-Write-LogHeader "Selecting Asset"
+    # Validation (for demonstration, expand as needed)
+    if (-not $p_Metadata.PackageName -or -not $p_Metadata.Repo -or -not $p_Metadata.Url -or -not $p_Metadata.Version -or -not $p_Metadata.Author -or -not $p_Metadata.Description) {
+        Write-Error "Missing mandatory metadata for nuspec file."
+        return
+    }
 
-# Select the best asset based on supported types
-$selectedAsset = Select-Asset -assets $latestReleaseInfo.assets
-if ($null -eq $selectedAsset) {
-    exit 1
-}
-
-# Determine file type from asset name
-$fileType = if ($selectedAsset.name -match '\.(exe|msi|zip|7z|msu|msp)$') { $matches[1] } else { $null }
-
-
-# Determine silent installation arguments based on file type
-$silentArgs = Get-SilentArgs -fileType $fileType
-
-# Extract package metadata
-$packageName = $latestReleaseInfo.name
-$version = $latestReleaseInfo.tag_name
-$author = $latestReleaseInfo.author.login
-$description = $latestReleaseInfo.body -replace "\r\n", " "
-$url = $selectedAsset.browser_download_url
-
-Write-Host "Download URL: $url"
-
-# Explicitly set the tools directory
-$toolsDir = Join-Path (Get-Location) "tools"
-
-# Create the tools directory
-New-Item -Path $toolsDir -ItemType "directory" -Force
-
-# Create a nuspec file for the package
-$nuspec = @"
+    $f_nuspec = @"
 <?xml version="1.0"?>
 <package xmlns="http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd">
   <metadata>
-    <id>$packageName</id>
-    <title>$packageName</title>
-    <version>$version</version>
-    <authors>$author</authors>
-    <owners>$author</owners>
-    <description>$description</description>
-    <projectUrl>$repo</projectUrl>
-    <packageSourceUrl>$url</packageSourceUrl>
-    <licenseUrl>$repo/blob/master/LICENSE</licenseUrl>
-    <tags>protonvpn vpn</tags>
+    <id>$($p_Metadata.PackageName)</id>
+    <title>$($p_Metadata.Repo)</title>
+    <version>$($p_Metadata.Version)</version>
+    <authors>$($p_Metadata.Author)</authors>
+    <owners>$($p_Metadata.Author)</owners>
+    <description>$($p_Metadata.Description)</description>
+    <projectUrl>$($p_Metadata.Repo)</projectUrl>
+    <packageSourceUrl>$($p_Metadata.Url)</packageSourceUrl>
+    <licenseUrl>$($p_Metadata.Repo)/blob/master/LICENSE</licenseUrl>
+    <tags></tags>
   </metadata>
 </package>
 "@
-# Create a nuspec file for the package
-$nuspecPath = Join-Path $toolsDir "$githubRepo.nuspec"
-Out-File -InputObject $nuspec -FilePath $nuspecPath -Encoding utf8
-Write-Host "Nuspec file created at: $nuspecPath"
+    # TODO: Specify the path to the nuspec file to be one above the tools directory
+    $f_nuspecPath = Join-Path $p_packageDir "$($p_Metadata.PackageName).nuspec"
+    try {
+        Out-File -InputObject $f_nuspec -FilePath $f_nuspecPath -Encoding utf8
+    }
+    catch {
+        Write-Error "Failed to create nuspec file at: $f_nuspecPath"
+        exit 1
+    }
+    return $f_nuspecPath
+}
+function New-InstallScript {
+    param (
+        [Parameter(Mandatory=$true)]
+        [PSCustomObject]$p_Metadata,
 
-$installScriptContent = @"
-$ErrorActionPreference = 'Stop';
+        [Parameter(Mandatory=$true)]
+        [string]$p_toolsDir
+    )
 
-# Prepare Chocolatey package arguments
-$packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = $fileType
-  url           = $url
-  softwareName  = "$githubRepo*"
-  silentArgs    = $silentArgs
-  validExitCodes= @(0)
+    # Validation
+    if (-not $p_Metadata.PackageName -or -not $p_Metadata.Repo -or -not $p_Metadata.Url -or -not $p_Metadata.Version -or -not $p_Metadata.Author -or -not $p_Metadata.Description) {
+        Write-Error "Missing mandatory metadata for install script."
+        return
+    }
+    
+    $f_installScriptContent = @"
+`$ErrorActionPreference = 'Stop';
+
+`$packageArgs = @{
+    packageName   = "$($p_Metadata.PackageName)"
+    fileType      = "$($p_Metadata.FileType)"
+    url           = "$($p_Metadata.Url)"
+    softwareName  = "$($p_Metadata.PackageName)"
+    silentArgs    = "$($p_Metadata.SilentArgs)"
+    validExitCodes= @(0)
 }
 Install-ChocolateyPackage @packageArgs
 "@
-Set-Location -Path $toolsDir
-Out-File -InputObject $installScriptContent -FilePath ".\chocolateyInstall.ps1" -Encoding utf8
+    # TODO: Specify the path to the install script to be one above the tools directory
+    $f_installScriptPath = Join-Path $p_toolsDir "chocolateyInstall.ps1"
+    Out-File -InputObject $f_installScriptContent -FilePath $f_installScriptPath -Encoding utf8
+    return $f_installScriptPath
+}
+#endregion
+###################################################################################################
+Write-LogHeader "Fetching Latest Release Info"
+#region Get Latest Release Info
 
+# TODO Replace URL with variable
+$repo = "https://github.com/maah/ProtonVPN-win-app"
+$githubUser = $repo.Split('/')[3]
+$githubRepo = $repo.Split('/')[4]
+$latestReleaseUrl = "https://api.github.com/repos/${githubUser}/${githubRepo}/releases/latest"
+
+# Fetch repository description
+$description = Get-RepoDescription -p_latestReleaseUrl $latestReleaseUrl
+
+# Display repository description
+Write-Host "Repository Description: $description"
+
+# Fetch latest release information
+Write-Host "Fetching latest release information from GitHub..."
+$latestReleaseInfo = Get-LatestReleaseInfo -p_latestReleaseUrl $latestReleaseUrl
+Write-Host "Latest Release URL: $latestReleaseUrl"
+
+#endregion
+###################################################################################################
+Write-LogHeader "Getting Asset Info"
+#region Get Asset Info
+
+# Select the best asset based on supported types
+Write-Host "Selecting asset..." 
+$selectedAsset = Select-Asset -p_assets $latestReleaseInfo.assets
+# Create package metadata object
+$packageMetadata        = [PSCustomObject]@{
+    PackageName         = $selectedAsset.name -replace '\.[^.]+$'
+    Version             = $latestReleaseInfo.tag_name
+    Author              = $latestReleaseInfo.author.login
+    Description         = $description
+    VersionDescription  = $latestReleaseInfo.body -replace "\r\n", " "
+    Url                 = $selectedAsset.browser_download_url
+    Repo                = $repo
+    FileType            = $fileType
+    SilentArgs          = $silentArgs
+}
+Write-Host "Selected asset: $($packageMetadata.PackageName)" -ForegroundColor Cyan
+
+# If the name contains the version number exactly, remove the version number from the package name
+if ($packageMetadata.PackageName -match $packageMetadata.Version) {
+    $packageMetadata.PackageName = $packageMetadata.PackageName -replace $packageMetadata.Version, ''
+}
+
+# Determine file type from asset name
+Write-Host "Determining file type from asset name..."
+$fileType = Get-Filetype -p_fileName $selectedAsset.name
+Write-Host "File type: $fileType" -ForegroundColor Cyan
+
+# Determine silent installation arguments based on file type
+Write-Host "Determining silent installation arguments for $fileType... (" -NoNewline; Write-Host "poorly" -ForegroundColor Yellow -NoNewline; Write-Host ")"
+$silentArgs = Get-SilentArgs -p_fileType $fileType
+Write-Host "Silent installation arguments for {$fileType}: $silentArgs" -ForegroundColor Cyan
+
+Write-Host
+Write-Host "Download URL: " -NoNewline
+Write-Host "$($packageMetadata.Url)" -ForegroundColor Blue
+Write-Host
+
+#endregion
+###################################################################################################
+<# Json Debugging
+Write-LogHeader "JSON Debugging"
+#region JSON Debugging
+# Function to format and print JSON recursively, allowing for nested lists
+function Format-Json {
+    param (
+        [Parameter(Mandatory=$true)]
+        [psobject]$json,
+        
+        [Parameter(Mandatory=$false)]
+        [int]$depth = 0
+    )
+
+    # Color scale from dark blue to light blue
+    $colorScale = @('DarkBlue', 'Blue', 'RoyalBlue', 'DodgerBlue', 'LightSkyBlue')
+
+    if ($null -eq $json) {
+        Write-Host "Received null json object."
+        return
+    }
+
+    $currentColor = $colorScale[$depth % $colorScale.Length]
+
+    if ($json -is [pscustomobject]) {
+        $properties = $json.PSObject.Properties
+        if ($null -eq $properties) {
+            Write-Host "No properties found in object."
+            return
+        }
+
+        $properties | ForEach-Object {
+            if ($null -eq $_) {
+                Write-Host "Null property found."
+                return
+            }
+
+            Write-Host $_.Name -ForegroundColor $currentColor -NoNewline
+            if ($_.Value -is [pscustomobject] -or $_.Value -is [System.Collections.ArrayList]) {
+                Write-Host ":"
+                Format-Json -json $_.Value -depth ($depth + 1)
+            } else {
+                Write-Host ": $($_.Value)" -ForegroundColor White
+            }
+        }
+    } elseif ($json -is [System.Collections.ArrayList]) {
+        $json | ForEach-Object {
+            if ($_ -is [pscustomobject] -or $_ -is [System.Collections.ArrayList]) {
+                Format-Json -json $_ -depth ($depth + 1)
+            } else {
+                Write-Host $_ -ForegroundColor White
+            }
+        }
+    } else {
+        Write-Host "Unsupported type: $($json.GetType().FullName)"
+    }
+}
+
+Write-Host "Latest Release Info:"
+Format-Json $latestReleaseInfo
+#endregion
+#>
+###################################################################################################
+Write-LogHeader "Creating Nuspec File and Install Script"
+#region Create Nuspec File and Install Script
+
+# Set the path to the package directory
+$packageDir = Join-Path (Get-Location).Path $packageMetadata.PackageName
+
+Write-Host "Checking for package directory..."
+# Create the tools directory if it doesn't exist
+if (-not (Test-Path $packageDir)) {
+    Write-Host "No pacakge directory found, creating pacakge directory..."
+    New-Item -Path $packageDir -ItemType Directory | Out-Null
+    Write-Host "Pacakge directory created at: $packageDir" -ForegroundColor Cyan
+}
+else {
+    Write-Host "Tools directory found at: $toolsDir" -ForegroundColor Cyan
+}
+
+# Explicitly set the path to the tools directory
+$toolsDir = Join-Path $packageDir "tools"
+
+Write-Host "Checking for tools directory..."
+# Create the tools directory if it doesn't exist
+if (-not (Test-Path $toolsDir)) {
+    Write-Host "No tools directory found, creating tools directory..."
+    New-Item -Path $toolsDir -ItemType Directory | Out-Null
+    Write-Host "Tools directory created at: $toolsDir" -ForegroundColor Cyan
+}
+else {
+    Write-Host "Tools directory found at: $toolsDir" -ForegroundColor Cyan
+}
+
+Write-Host "Creating nuspec file..."
+$nuspecPath = New-NuspecFile -p_Metadata $packageMetadata -p_packageDir $packageDir
+Write-Host "Nuspec file created at: $nuspecPath" -ForegroundColor Cyan
+
+Write-Host "Creating install script..."
+$installScriptPath = New-InstallScript -p_Metadata $packageMetadata -p_toolsDir $toolsDir
+Write-Host "Install script created at: $installScriptPath" -ForegroundColor Cyan
+
+#endregion
+###################################################################################################
 Write-LogHeader "Creating Chocolatey Package"
-Write-Host "Tools Directory: $toolsDir"
-Write-Host "Nuspec Path: $nuspecPath"
-Write-Host "Working Directory: $(Get-Location)"
+#region Create Chocolatey Package
 
 # Check for Nuspec File
+Write-Host "Checking for nuspec file..."
 if (-not (Test-Path $nuspecPath)) {
     Write-Error "Nuspec file not found at: $nuspecPath"
     exit 1
 }
 else {
-    Write-Host "Nuspec file found at: $nuspecPath"
+    Write-Host "Nuspec file found at: $nuspecPath" -ForegroundColor Cyan
 }
 
 # Create Chocolatey package
 try {
-    # Move to the directory containing the .nuspec file
-    Set-Location -Path $toolsDir
-    write-host "Moved to: $(Get-Location)"
-    # Create the Chocolatey package
     Write-Host "Creating Chocolatey package..."
-    choco pack $nuspecPath -Force -Verbose
+    choco pack $nuspecPath -Force -Verbose --out $packageDir
 } catch {
     Write-Error "Failed to create Chocolatey package."
     exit 1
 }
+
+#endregion
+###################################################################################################
