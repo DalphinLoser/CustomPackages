386c311f994fe654514ea60cf5957046e7d6f9b5
GitHub Action <action@github.com>
2023-09-22 07:05:49 +0000

    Update packages

--- packages/ProtonVPN_v/tools/chocolateyInstall.ps1
+++ packages/ProtonVPN_v/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
     packageName   = "ProtonVPN_v"
     fileType      = "exe"
     url           = "https://github.com/maah/ProtonVPN-win-app/releases/download/3.1.1/ProtonVPN_v3.1.1.exe"
-    softwareName  = "ProtonVPN_v"
+    softwareName  = "ProtonVPN-win-app"
     silentArgs    = "/S /s /Q /q /SP- /VERYSILENT /NORESTART /quiet /silent"
     validExitCodes= @(0)
 }
