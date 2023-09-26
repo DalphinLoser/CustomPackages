29f76310980787b90df395ba8b697bac5d8cc8af
GitHub Action <action@github.com>
2023-09-26 18:33:06 +0000

    Update packages

--- packages/Ryujinx.release-channel-master.testavaryujinxwinx64/tools/chocolateyInstall.ps1
+++ packages/Ryujinx.release-channel-master.testavaryujinxwinx64/tools/chocolateyInstall.ps1
@@ -1,9 +1,10 @@
 $ErrorActionPreference = 'Stop';
+$toolsDir   = Join-Path $(Get-ToolsLocation) $env:ChocolateyPackageName
 
 $packageArgs = @{
     packageName     = "Ryujinx.release-channel-master.testavaryujinxwinx64"
     url             = "https://github.com/Ryujinx/release-channel-master/releases/download/1.1.1032/test-ava-ryujinx-1.1.1032-win_x64.zip"
-    unzipLocation   = "C:\Users\runneradmin\\AutoPackages\\Ryujinx.release-channel-master.testavaryujinxwinx64"
+    unzipLocation   = $toolsDir
 }
 
 Install-ChocolateyZipPackage @packageArgs
