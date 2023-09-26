8dcdc2de81c8754b9a8e4486a8c749a367247a6c
GitHub Action <action@github.com>
2023-09-26 22:00:17 +0000

    Update packages

--- packages/BlackPearlOrigin.blackpearlorigin/tools/chocolateyInstall.ps1
+++ packages/BlackPearlOrigin.blackpearlorigin/tools/chocolateyInstall.ps1
@@ -1,11 +1,12 @@
 $ErrorActionPreference = 'Stop';
 
 $packageArgs = @{
-    packageName   = "BlackPearlOrigin.blackpearlorigin"
-    fileType      = "msi"
-    url           = "https://github.com/BlackPearlOrigin/blackpearlorigin/releases/download/1.1.0/Black.Pearl.Origin_0.3.0_x64_en-US.msi"
-    softwareName  = "blackpearlorigin"
-    silentArgs    = "/quiet /qn /norestart"
-    validExitCodes= @(0)
+    packageName     = "BlackPearlOrigin.blackpearlorigin"
+    fileType        = "msi"
+    url             = "https://github.com/BlackPearlOrigin/blackpearlorigin/releases/download/1.1.0/Black.Pearl.Origin_0.3.0_x64_en-US.msi"
+    softwareName    = "BlackPearlOrigin"
+    silentArgs      = "/quiet /qn /norestart"
+    validExitCodes  = @(0)
 }
+
 Install-ChocolateyPackage @packageArgs
