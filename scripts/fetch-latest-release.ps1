b21145fbed9353f535725a37b396093d85e6b230
DalphinLoser <63314427+DalphinLoser@users.noreply.github.com>
2023-09-26 01:29:44 -0400

    Update fetch-latest-release.ps1

--- scripts/fetch-latest-release.ps1
+++ scripts/fetch-latest-release.ps1
@@ -439,7 +439,7 @@ function Get-Updates {
         if ($latestReleaseUrl -ne $packageSourceUrl) {
             Write-Host "Updating metadata for $package"
             # Get the asset metadata
-            Initialize-URLs -p_repoUrl $latestReleaseUrl -p_specifiedAssetName $specifiedAssetName
+            Initialize-URLs -p_repoUrl $latestReleaseUrl
             $myMetadata = Get-AssetInfo -latestReleaseInfo $latestReleaseInfo -specifiedAssetName $specifiedAssetName
             # Create the nuspec file and install script
             $nuspecPath = New-NuspecFile -p_Metadata $myMetadata -p_packageDir "$f_packageDir/$package"
