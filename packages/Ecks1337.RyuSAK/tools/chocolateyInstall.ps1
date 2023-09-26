3ecfdf92a4b6f25f40b15c7331dfa769e8e2fae4
GitHub Action <action@github.com>
2023-09-26 17:07:46 +0000

    Update packages

---
+++ packages/Ecks1337.RyuSAK/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+    packageName     = "Ecks1337.RyuSAK"
+    fileType        = "exe"
+    url             = "https://github.com/Ecks1337/RyuSAK/releases/download/v1.6.2/RyuSAK-1.6.2.Setup.exe"
+    softwareName    = "RyuSAK"
+    silentArgs      = "/S /s /Q /q /SP- /VERYSILENT /NORESTART /quiet /silent"
+    validExitCodes  = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
