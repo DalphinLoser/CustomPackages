ff24f2c0a4ea56a094a04c70a9275bcff982802d
GitHub Action <action@github.com>
2023-09-27 02:59:39 +0000

    Update packages

---
+++ packages/Ryujinx.release-channel-master.testavaryujinxwinx64/tools/chocolateyUninstall.ps1
@@ -0,0 +1,10 @@
+$f_installDir = "C:\Program Files\Ryujinx.release-channel-master.testavaryujinxwinx64"
+$shortcutPath = "C:\Users\runneradmin\Desktop"
+
+# Remove the installation directory
+if (Test-Path $f_installDir) {
+    Remove-Item -Path $f_installDir -Recurse -Force
+}
+
+# Remove any shortcuts related to this package from the Desktop
+Get-ChildItem -Path $shortcutPath -Filter "Ryujinx.release-channel-master.testavaryujinxwinx64*.lnk" | Remove-Item -Force
