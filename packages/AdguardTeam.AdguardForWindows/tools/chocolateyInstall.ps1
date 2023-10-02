11875608d6be3cf24aa396112f73d66a21de5c5f
GitHub Action <action@github.com>
2023-10-02 06:52:26 +0000

    Update packages

--- packages/AdguardTeam.AdguardForWindows/tools/chocolateyInstall.ps1
+++ packages/AdguardTeam.AdguardForWindows/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
     packageName     = "adguardteam.adguardforwindows"
     fileType        = "exe"
     url             = "https://github.com/AdguardTeam/AdguardForWindows/releases/download/v7.14.0/AdGuard-7.14.exe"
-    softwareName    = "AdguardTeam"
+    softwareName    = "AdguardForWindows"
     silentArgs      = "/S /s /Q /q /SP- /VERYSILENT /NORESTART /quiet /silent"
     validExitCodes  = @(0)
 }
