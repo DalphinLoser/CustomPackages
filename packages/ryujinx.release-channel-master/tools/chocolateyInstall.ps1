1ac0d3101d5b4313a1d48fe58cd8cf9942dca53d
GitHub Action <action@github.com>
2023-10-04 21:26:25 +0000

    Update packages

--- packages/ryujinx.release-channel-master/tools/chocolateyInstall.ps1
+++ packages/ryujinx.release-channel-master/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@ $toolsDir   = "C:\AutoPackages\ryujinx.release-channel-master"
 
 $packageArgs = @{
     packageName     = "ryujinx.release-channel-master"
-    url             = "https://github.com/Ryujinx/release-channel-master/releases/download/1.1.1038/test-ava-ryujinx-1.1.1038-win_x64.zip"
+    url             = "https://github.com/Ryujinx/release-channel-master/releases/download/1.1.1040/test-ava-ryujinx-1.1.1040-win_x64.zip"
     unzipLocation   = $toolsDir
 }
 
