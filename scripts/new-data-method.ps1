7297840c217b504a1429a285c7bb1e96bcea9029
DalphinLoser <63314427+DalphinLoser@users.noreply.github.com>
2023-10-10 13:39:38 -0400

    Saves icon in repo

--- scripts/new-data-method.ps1
+++ scripts/new-data-method.ps1
@@ -62,6 +62,73 @@ Log=    $($logPath)
         } catch {
             Write-Error "Failed to remove temporary directory: $_"
         }
+        
+        # PSCustonObject to hold the extracted data 
+        $versionInfo = Get-VersionInfo -FilePath "$($metadataPath)\VERSIONINFO.rc"
+
+        # Move the icon into the icons directory. If the directory doesn't exist, create it. It should be located in the root directory
+        Write-DebugLog "    The Current Content of the Icons Directory is: " -NoNewline -ForegroundColor Yellow
+        Write-DebugLog (Get-ChildItem -Path $iconPath -ErrorAction SilentlyContinue)
+
+
+        # Path to current .ico. Find it in the icon directory by looking for any file with the .ico extension
+        $currentIconPath = Get-ChildItem -Path $iconPath -Filter "*.ico" -Recurse -ErrorAction SilentlyContinue
+        Write-DebugLog "    The Current Icon Path is: " -NoNewline -ForegroundColor Yellow
+        Write-DebugLog $currentIconPath.FullName
+
+        # If the icons directory doesn't exist, create 
+        $newIconPath = $rootDir + "\icons"
+        if (-not (Test-Path $newIconPath)) {
+            New-Item -Path $newIconPath -ItemType Directory -Force -ErrorAction Stop
+        }
+        # Move the icon to the icons directory and rename it to the product name
+        Move-Item -Path $currentIconPath.FullName -Destination "$($newIconPath)\$($versionInfo.ProductName).ico" -Force -ErrorAction Stop
+
+        # Remove everything in the resources\RH-Get directory except for the resource_hacker folder and its contents
+        $RHGetDir = Get-ChildItem -Path "$($rootDir)\resources\RH-Get" -Exclude "resource_hacker"
+        foreach ($item in $RHGetDir) {
+            Write-DebugLog "Removing " -NoNewline -ForegroundColor Yellow
+            Write-DebugLog "$($item.FullName)"
+            Remove-Item -Path $item.FullName -Recurse -Force
+        }
+
+        return $versionInfo
+            
+    } catch {
+        # Handle errors and exceptions
+        Write-Error "An error occurred: $_"
+    }
+}
+function Get-VersionInfo {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$FilePath
+    )
+
+    try {
+        # Check if the file exists
+        if (-not (Test-Path -Path $FilePath -PathType Leaf)) {
+            throw "File not found at path: $FilePath"
+        }
+
+        # Read the content of the file
+        $fileContent = Get-Content -Path $FilePath -Raw
+
+        # Initialize a hashtable to store the extracted values
+        $versionInfo = @{}
+
+        # Extract other values inside the BLOCK "StringFileInfo"
+        if ($fileContent -match '(?s)BLOCK "StringFileInfo"(.*?)BLOCK "VarFileInfo"') {
+            $stringFileInfo = $matches[1]
+            $stringFileInfo -split "`n" | ForEach-Object {
+                if ($_ -match 'VALUE "(.+?)", "(.+?)"') {
+                    $versionInfo[$matches[1]] = $matches[2].Trim()
+                }
+            }
+        }        
+
+        # Return the extracted values
+        return $versionInfo
 
     } catch {
         # Handle errors and exceptions
