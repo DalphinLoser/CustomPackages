8077ab047e4dd469b1da38303151d686168128a5
GitHub Action <action@github.com>
2023-10-11 21:02:25 +0000

    Automated Package Creation: https://github.com/LizardByte/Sunshine

---
+++ packages/lizardbyte.sunshine/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+    packageName     = "lizardbyte.sunshine"
+    fileType        = "exe"
+    url             = "https://github.com/LizardByte/Sunshine/releases/download/v0.20.0/sunshine-windows-installer.exe"
+    softwareName    = "Sunshine"
+    silentArgs      = "/S /s /Q /q /SP- /VERYSILENT /NORESTART /quiet /silent"
+    validExitCodes  = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
