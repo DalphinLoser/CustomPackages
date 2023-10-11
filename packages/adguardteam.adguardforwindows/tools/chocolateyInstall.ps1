cb8948642a2f8817902caa91ebd1b84c3017abf8
GitHub Action <action@github.com>
2023-10-11 20:31:38 +0000

    Automated Package Creation: https://github.com/AdguardTeam/AdguardForWindows/releases/tag/v7.15.0

---
+++ packages/adguardteam.adguardforwindows/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+    packageName     = "adguardteam.adguardforwindows"
+    fileType        = "exe"
+    url             = "https://github.com/AdguardTeam/AdguardForWindows/releases/download/v7.15.0/AdGuard-7.15-.4385.exe"
+    softwareName    = "AdGuard"
+    silentArgs      = "/S /s /Q /q /SP- /VERYSILENT /NORESTART /quiet /silent"
+    validExitCodes  = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
