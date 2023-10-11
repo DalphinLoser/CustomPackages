8a5872fccfe4296480908f4dc460d186eb200506
GitHub Action <action@github.com>
2023-10-11 20:30:33 +0000

    Automated Package Creation: https://github.com/maah/ProtonVPN-win-app

--- packages/maah.protonvpn-win-app/tools/chocolateyInstall.ps1
+++ packages/maah.protonvpn-win-app/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
     packageName     = "maah.protonvpn-win-app"
     fileType        = "exe"
     url             = "https://github.com/maah/ProtonVPN-win-app/releases/download/3.2.1/ProtonVPN_v3.2.1.exe"
-    softwareName    = "ProtonVPN"
+    softwareName    = "Proton VPN"
     silentArgs      = "/S /s /Q /q /SP- /VERYSILENT /NORESTART /quiet /silent"
     validExitCodes  = @(0)
 }
