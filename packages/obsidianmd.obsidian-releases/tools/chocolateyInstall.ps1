45315d5bdf3f17d7cc6a862166faab98be5518e9
DalphinLoser <63314427+DalphinLoser@users.noreply.github.com>
2023-10-17 00:54:36 -0400

    revert for test

--- packages/obsidianmd.obsidian-releases/tools/chocolateyInstall.ps1
+++ packages/obsidianmd.obsidian-releases/tools/chocolateyInstall.ps1
@@ -3,11 +3,10 @@ $ErrorActionPreference = 'Stop';
 $packageArgs = @{
     packageName     = "obsidianmd.obsidian-releases"
     fileType        = "exe"
-    url             = "https://github.com/obsidianmd/obsidian-releases/releases/download/v1.4.16/Obsidian.1.4.16-32.exe"
+    url             = "https://github.com/obsidianmd/obsidian-releases/releases/download/v1.4.14/Obsidian.1.4.14-32.exe"
     softwareName    = "Obsidian"
     silentArgs      = "/S /s /Q /q /SP- /VERYSILENT /NORESTART /quiet /silent"
     validExitCodes  = @(0)
 }
 
 Install-ChocolateyPackage @packageArgs
-
