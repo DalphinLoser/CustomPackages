84f015d4e961aa343a80685569fbb376a0968576
GitHub Action <action@github.com>
2023-10-26 12:21:41 +0000

    Automatically Updated: test-ava-ryujinx-1.1.1065-win_x64.zip

--- packages/ryujinx.release-channel-master.test-ava-ryujinx-win_x64/tools/chocolateyInstall.ps1
+++ packages/ryujinx.release-channel-master.test-ava-ryujinx-win_x64/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@ $toolsDir   = "C:\AutoPackages\ryujinx.release-channel-master.test-ava-ryujinx-w
 
 $packageArgs = @{
     packageName     = "ryujinx.release-channel-master.test-ava-ryujinx-win_x64"
-    url             = "https://github.com/Ryujinx/release-channel-master/releases/download/1.1.1064/test-ava-ryujinx-1.1.1064-win_x64.zip"
+    url             = "https://github.com/Ryujinx/release-channel-master/releases/download/1.1.1065/test-ava-ryujinx-1.1.1065-win_x64.zip"
     unzipLocation   = $toolsDir
 }
 
@@ -35,4 +35,5 @@ foreach ($exe in $exes) {
     $DesktopShortcut.TargetPath = $exe.FullName
     $StartMenuShortcut.Save()
 }
+
 
