aa16d7bdcd12148aadd1b469e2d7b98cc37c0e46
GitHub Action <action@github.com>
2023-11-02 00:51:50 +0000

    Automatically Updated:

--- packages/y0urd34th.project-gld/tools/chocolateyInstall.ps1
+++ packages/y0urd34th.project-gld/tools/chocolateyInstall.ps1
@@ -3,10 +3,11 @@ $ErrorActionPreference = 'Stop';
 $packageArgs = @{
     packageName     = "y0urd34th.project-gld"
     fileType        = "exe"
-    url             = "https://github.com/Y0URD34TH/Project-GLD/releases/download/Hotfix-V2.02/Project-GLD.exe"
+    url             = "https://github.com/Y0URD34TH/Project-GLD/releases/download/Update-V2.07/Project-GLD.exe"
     softwareName    = "Project-GLD"
     silentArgs      = "/S /s /Q /q /quiet /silent"
     validExitCodes  = @(0)
 }
 
 Install-ChocolateyPackage @packageArgs
+
