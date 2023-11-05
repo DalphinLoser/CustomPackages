2212f1268a68b470305b851063277a25ff9a19c1
GitHub Action <action@github.com>
2023-11-05 12:19:46 +0000

    Automatically Updated:

--- packages/ryujinx.release-channel-master.test-ava-ryujinx-win_x64/tools/chocolateyInstall.ps1
+++ packages/ryujinx.release-channel-master.test-ava-ryujinx-win_x64/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@ $toolsDir   = "C:\AutoPackages\ryujinx.release-channel-master.test-ava-ryujinx-w
 
 $packageArgs = @{
     packageName     = "ryujinx.release-channel-master.test-ava-ryujinx-win_x64"
-    url             = "https://github.com/Ryujinx/release-channel-master/releases/download/1.1.1069/test-ava-ryujinx-1.1.1069-win_x64.zip"
+    url             = "https://github.com/Ryujinx/release-channel-master/releases/download/1.1.1070/test-ava-ryujinx-1.1.1070-win_x64.zip"
     unzipLocation   = $toolsDir
 }
 
@@ -37,4 +37,5 @@ $startMenuShortcutPath = Join-Path $startMenuDir "$exeName.lnk"
 $StartMenuShortcut = $WshShell.CreateShortcut($startMenuShortcutPath)
 $DesktopShortcut.TargetPath = $largestExe.FullName
 $StartMenuShortcut.Save()
+
 
