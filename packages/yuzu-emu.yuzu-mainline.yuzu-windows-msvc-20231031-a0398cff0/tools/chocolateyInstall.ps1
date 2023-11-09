e6788c687a36d963d090da7fa2bc294d47efef5e
GitHub Action <action@github.com>
2023-11-09 00:52:50 +0000

    Automatically Updated:

--- packages/yuzu-emu.yuzu-mainline.yuzu-windows-msvc-20231031-a0398cff0/tools/chocolateyInstall.ps1
+++ packages/yuzu-emu.yuzu-mainline.yuzu-windows-msvc-20231031-a0398cff0/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@ $toolsDir   = "C:\AutoPackages\yuzu-emu.yuzu-mainline.yuzu-windows-msvc-20231031
 
 $packageArgs = @{
     packageName     = "yuzu-emu.yuzu-mainline.yuzu-windows-msvc-20231031-a0398cff0"
-    url             = "https://github.com/yuzu-emu/yuzu-mainline/releases/download/mainline-0-1613/yuzu-windows-msvc-20231107-4a1c10848.7z"
+    url             = "https://github.com/yuzu-emu/yuzu-mainline/releases/download/mainline-0-1614/yuzu-windows-msvc-20231108-ecdd38633.7z"
     unzipLocation   = $toolsDir
 }
 
@@ -37,6 +37,7 @@ $startMenuShortcutPath = Join-Path $startMenuDir "$exeName.lnk"
 $StartMenuShortcut = $WshShell.CreateShortcut($startMenuShortcutPath)
 $DesktopShortcut.TargetPath = $largestExe.FullName
 $StartMenuShortcut.Save()
+
 
 
 
