6d293bf6d725c2f89eea73d63ec974e97a2ab509
DalphinLoser <63314427+DalphinLoser@users.noreply.github.com>
2024-01-10 16:21:43 -0500

    Update tempExtractPath to use the system's temporary directory

--- scripts/auto-package-updates.ps1
+++ scripts/auto-package-updates.ps1
@@ -64,8 +64,8 @@ function Get-Updates {
             # Remove the extension to use it as a directory name
             $randomDirName = $randomFileName -replace "\..*$", ""
 
-            # Create a path to the temp directory under the project root
-            $tempExtractPath = Join-Path -Path $PSScriptRoot -ChildPath $randomDirName
+            # Combine the temp path with the random directory name.
+            $tempExtractPath = Join-Path -Path $env:TEMP -ChildPath $randomDirName
             # Ensure the directory is created
             New-Item -ItemType Directory -Path $tempExtractPath -Force | Out-Null
             Write-DebugLog "Created Temp Directory: $($tempExtractPath)"
@@ -134,7 +134,7 @@ function Get-Updates {
             foreach ($entry in $entries) {
                 # Extract the file
                 Write-DebugLog "Extracting file: $($entry.FullName) to: $tempExtractPath"
-                [System.IO.Compression.ZipFileExtensions]::ExtractToFile($entry, $tempExtractPath, $true)
+                [System.IO.Compression.ZipFileExtensions]::ExtractToFile($entry, $tempExtractPath)
                 Write-DebugLog "Extracted file: $($entry.FullName) to: $tempExtractPath"
             }
 
