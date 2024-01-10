bf4e91924edeb90d3a00bd135b281641237fe48c
DalphinLoser <63314427+DalphinLoser@users.noreply.github.com>
2024-01-10 18:59:57 -0500

    Update package creation process

--- scripts/package-functions.ps1
+++ scripts/package-functions.ps1
@@ -268,7 +268,7 @@ function New-ChocolateyPackage {
         # Create a new empty directory, then move it to the package directory
         $tempDir = New-Item -Path "$env:TEMP" -Name "chocolateyTemp" -ItemType Directory -Force
         # Create the package
-        New-ChocolateyPackage $NuspecPath -OutputDirectory $tempDir
+        $output = choco pack $NuspecPath --debug --out $tempDir
         # Set tempPackagePath to the path of the package (the only nupkg file in the temp directory)
         $tempPackagePath = Get-ChildItem -Path $tempDir -Filter *.nupkg -Recurse | Select-Object -ExpandProperty FullName
         # Move the package to the package directory
