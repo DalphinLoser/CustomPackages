df0c3d1910f23839dabb5f1600d14e3fe0375c13
DalphinLoser <63314427+DalphinLoser@users.noreply.github.com>
2024-01-10 16:15:33 -0500

    Fix file extraction method in auto-package-updates.ps1

--- scripts/auto-package-updates.ps1
+++ scripts/auto-package-updates.ps1
@@ -134,7 +134,7 @@ function Get-Updates {
             foreach ($entry in $entries) {
                 # Extract the file
                 Write-DebugLog "Extracting file: $($entry.FullName) to: $tempExtractPath"
-                [System.IO.Compression.FileSystem]::ExtractToFile($entry, $tempExtractPath, $true)
+                [System.IO.Compression.ZipFileExtensions]::ExtractToFile($entry, $tempExtractPath, $true)
                 Write-DebugLog "Extracted file: $($entry.FullName) to: $tempExtractPath"
             }
 
